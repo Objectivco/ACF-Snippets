855506c2e5054d52798ba2ae83aa553ffdb4e84d
Wesley Cole <wes@cgd.io>
2016-12-15 10:44:45 -0500

    removing generated php files for testing

--- partials/sections/content/item.php
+++
@@ -1,7 +0,0 @@
-<section class="content-section page-flexible-section <?php echo $padding_classes; ?>">
-	<div class="wrap wrap_thin inside-content-wrap">
-		<div class="content">
-			<?php echo $content; ?>
-		</div>
-	</div>
-</section>
